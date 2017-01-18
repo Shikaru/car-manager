5497a329eca62b43f1cf079274b62d56c8cf6482
Armando Gonzalez <susana_0901@hotmail.com>
2017-01-18 02:54:57 -0400

    header.php agregado

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+	<link rel="stylesheet" type="text/css" href="/mystyle.css">
+	</head>
+	<body>
+	<?php include '/headerv02.php';?>
+	<br>
+	<div id="header">
+	<a href="../">Home</a>
+	<a href="/mgmVehicle.php">Administracion de Vehiculos</a>
+	<a href="<?php echo $_SERVER['HTTP_REFERER'];?>">Regresar</a>
+	</div><!--header-->
+
+
+<?php
+
+?>
+
+	</body>
+</html>
